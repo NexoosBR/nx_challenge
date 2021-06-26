fc39e00faf2d3bb3cc030fbc3dd27683e2f84dae
Renan Proença <renan.maringolo@gmail.com>
2021-06-25 21:03:48 -0300

    seeds & unit test

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'CRIANDO REQUESTER'
+5.times  do
+	Requester.create({
+		name: Faker::Book.title,
+		cnpj: Faker::Company.brazilian_company_number(formatted: true),
+    phone: Faker::PhoneNumber.phone_number_with_country_code,
+    email: Faker::Internet.email,
+    commercial_address: Faker::Address.full_address,
+    company_birth: Faker::Date.birthday(min_age: 18, max_age: 65)
+	})
+end
+
+puts 'CRIANDO CRÉDITO'
+5.times do
+  Credit.create({
+    amount: Faker::Number.decimal(l_digits: 2),
+    credit_at: Faker::Date.in_date_period,
+    accept: 's',
+    requester_id: FactoryBot.create(:requester).id
+  })
+end
+
+puts 'CRIANDO EMPRÉSTIMO'
+5.times do
+  Loan.create({
+    amount: Faker::Number.decimal(l_digits: 2),
+    tax: Faker::Number.decimal(l_digits: 1, r_digits: 2),
+    term: Faker::Number.non_zero_digit,
+    credit_id: FactoryBot.create(:credit).id
+  })
+end
--- spec/presenters/loan_presenter_spec.rb
+++ spec/presenters/loan_presenter_spec.rb
@@ -1,14 +1,18 @@
 require "rails_helper"
 
 describe LoanPresenter do
-  let(:loan) { double(name: 'Empresa de Teste Nacional') }
   subject { described_class.new(loan) }
+
+  let(:loan) { double(name: 'Empresa de Teste Nacional') }
 
   it { expect(loan.name).to eq('Empresa de Teste Nacional') }
 
   describe 'PMT' do
-    it 'returns calc payment' do
+    before do
       allow(subject).to receive(:calculate_pmt).and_return(:message)
+    end
+
+    it 'returns calc payment' do
       calc = subject.calculate_pmt
 
       expect(subject.calculate_pmt).to eq(calc)
