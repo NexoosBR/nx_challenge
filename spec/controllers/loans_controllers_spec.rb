425efc54b4b00a17c523d25d2d9c6e9e98a79461
Mário Jorge <mariojorgenet@gmail.com>
2021-07-13 08:17:48 -0300

    adicionando testes quando não é enviado dados aos controllers

--- spec/controllers/loans_controllers_spec.rb
+++ spec/controllers/loans_controllers_spec.rb
@@ -2,19 +2,39 @@ require "rails_helper"
 
 RSpec.describe LoansController do
   describe "GET show" do
-    it do
-      loan = Loan.create(value: 100, tax: 1, months: 3)
-      get :show, params: { id: loan.id }
-      param = JSON.parse(response.body).with_indifferent_access
-      expect(param[:loan][:id]).to(eq(loan.id))
+    context 'there is data' do
+      it 'return the id of a loan' do
+        loan = Loan.create(value: 100, tax: 1, months: 3)
+        get :show, params: { id: loan.id }
+        param = JSON.parse(response.body).with_indifferent_access
+        expect(param[:loan][:id]).to(eq(loan.id))
+      end
+    end
+
+    context 'there is no data' do
+      it 'return the id of a loan' do
+        get :show, params: { id: 99 }
+        param = JSON.parse(response.body).with_indifferent_access
+        expect(param[:loan]).to(eq(nil))
+      end
     end
   end
 
-  describe "POST show" do
-    it do
-      post :create, params: { value: 100, tax: 1, months: 3 }
-      param = JSON.parse(response.body).with_indifferent_access
-      expect(param[:loan][:id]).to(eq(Loan.order(:id).last.id))
+  describe "POST create" do
+    describe 'with data' do
+      it 'return last id saved in database' do
+        post :create, params: { value: 100, tax: 1, months: 3 }
+        param = JSON.parse(response.body).with_indifferent_access
+        expect(param[:loan][:id]).to(eq(Loan.order(:id).last.id))
+      end
+    end
+
+    describe 'with no data or wrong data' do
+      it 'return last id saved in database' do
+        post :create
+        param = JSON.parse(response.body).with_indifferent_access
+        expect(param[:loan][:id]).to(eq(nil))
+      end
     end
   end
 end
