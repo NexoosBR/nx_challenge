08e4c6042810f16f4462aeb3bac779368d89a77c
rodolforpr88 <rodolforpr88@gmail.com>
2021-07-18 21:31:54 -0300

    update the methods in controller to render the result as json e create the before action set_loan

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,10 +1,31 @@
 class LoansController < ApplicationController
+  before_action :set_loan, only: [:show]
+
   def create
-    render json: { loan: { id: 1 } }
+    loan = Loan.new(loan_params)
+
+    if loan.save
+      render json: { loan: { id: loan.id } }, status: :created
+    else
+      render json: loan.errors, status: :bad_request
+    end
   end
 
   def show
-    pmt =  3_700 / 12
-    render json: { loan: { id: 1, pmt: pmt } }
+    render json: { loan: { id: @loan.id, pmt: @loan.pmt } }, status: :ok
+  end
+
+  private
+
+  def set_loan
+    begin
+      @loan = Loan.find(params[:id])
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { error: e }, status: :not_found
+    end
+  end
+
+  def loan_params
+    params.permit(:value, :months, :taxa)
   end
 end
