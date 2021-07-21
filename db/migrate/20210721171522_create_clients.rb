53d1c3cc6ec35bda2a8dfae38443d61e54fe93ab
Lucas Dalbonio <dalbonio2@gmail.com>
2021-07-21 14:19:45 -0300

    client and loan migrations

--- db/migrate/20210721171522_create_clients.rb
+++ db/migrate/20210721171522_create_clients.rb
@@ -1,6 +1,11 @@
 class CreateClients < ActiveRecord::Migration[6.1]
   def change
-    create_table :clients do |t|
+      create_table :clients do |t|
+      
+      t.string :name
+      t.string :email
+      t.string :phone
+      t.string :cpf, index: { unique: true, name: 'unique_cpfs' }
 
       t.timestamps
     end
