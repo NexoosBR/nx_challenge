632b0f7d7e9890cacad9d2687fcdceff46af7eea
Lucas Dalbonio <dalbonio2@gmail.com>
2021-07-21 14:30:49 -0300

    client and loan controllers

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,17 @@
 class ClientsController < ApplicationController
+    def create
+        client = Client.new(client_params)
+        if client.valid?
+            client.save
+            render json: {client: client}, status: :created
+        else
+            render json: {errors: client.errors.full_messages}, status: :unprocessable_entity
+        end
+    end
+
+    private 
+    def client_params
+        params.require(:client).permit(:name, :email, :phone, :cpf)
+    end
+
 end
