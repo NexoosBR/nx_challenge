739d73fb1a178774599ddf18558099b0c3d2580b
Lucas Dalbonio <dalbonio2@gmail.com>
2021-07-21 14:26:19 -0300

    client and loan models

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,18 @@
+gem 'cpf_cnpj'
+
 class Client < ApplicationRecord
+    has_many :loans
+
+    validates :name, presence: true
+    validates :email, presence: true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: 'invalid email'}
+    validates :phone, presence: true
+    validates :cpf, uniqueness: true
+
+    validate :isCpfValid?
+
+    def isCpfValid?
+        if not CPF.valid? self.cpf
+            errors.add(:cpf, "invalid")
+        end
+    end
 end
