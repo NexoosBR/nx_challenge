d1da76a4b1d2324a8a5d4272d567ea105a9f7b65
matheusfelix <matheusgfelix@gmail.com>
2021-07-21 15:11:59 -0300

    Add call_pmt_calculation call test

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -7,4 +7,13 @@ RSpec.describe Loan, type: :model do
   it { is_expected.to validate_numericality_of(:value).is_greater_than(0) }
   it { is_expected.to validate_numericality_of(:rate).is_greater_than(0) }
   it { is_expected.to validate_numericality_of(:months_period).is_greater_than(0) }
+
+  describe '#call_pmt_calculation' do
+    let!(:loan) { create(:loan) }
+
+    it "should call pmt_calculation" do
+      expect(loan).to receive(:pmt_calculation)
+      loan.call_pmt_calculation
+    end
+  end
 end
