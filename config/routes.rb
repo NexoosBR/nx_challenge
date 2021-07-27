b7bcdaf2e9c0bba60b92e9cced04998d8c26b498
Náthaly Rocha <nathalyfroliveira@gmail.com>
2021-07-26 23:49:49 -0300

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :loans, only: [:show, :create] do
+  resources :clients, defaults: { format: :json } do
+    resources :loans #, only: [:show, :create,]
   end
 end
