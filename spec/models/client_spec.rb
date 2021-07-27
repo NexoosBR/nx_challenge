7fd81d9e9fa362537993b3ba66fa5588b9166f31
Luiz Raupp <rauppluizfr@gmail.com>
2021-07-27 09:47:13 -0300

    testing client model validation (email)

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Client, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is not valid without an email" do
+    client = Client.new(email: nil)
+    expect(client).to_not be_valid
+  end
+
+  it "is valid - email is ok" do
+    client = Client.new(email: "this_is_a_test@server.com")
+    expect(client).to be_valid
+  end
+
+  it "invalid - email not ok" do
+    client = Client.new(email: "this_is_a_test")
+    expect(client).to_not be_valid
+  end
 end
