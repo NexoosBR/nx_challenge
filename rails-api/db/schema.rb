3390d1b4987d98b1d55f1fad567c0b29b2c407bd
Leandro Lasnor <leandrolsnr@gmail.com>
2021-08-08 04:21:02 -0300

    Finishing

--- rails-api/db/schema.rb
+++ rails-api/db/schema.rb
@@ -10,15 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_08_045927) do
+ActiveRecord::Schema.define(version: 2021_08_06_160713) do
 
   create_table "loans", force: :cascade do |t|
     t.integer "value", null: false
     t.integer "months", null: false
     t.float "taxa", null: false
+    t.float "pmt", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.float "pmt", null: false
   end
 
 end
