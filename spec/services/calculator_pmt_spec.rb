7f07187d67b2b89d019fa7ee555aa21fc61f6a29
David Schnee <david.schnee1@gmail.com>
2021-08-09 20:14:35 -0300

    chore/ rubocop in calculator pmt spec

--- spec/services/calculator_pmt_spec.rb
+++ spec/services/calculator_pmt_spec.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 describe CalculatorPmtService do
-  describe "#call" do
+  describe '#call' do
     subject { described_class.new(amount: 1000, instalment_number: 3, tax: 2) }
     it { expect(subject.call).to eq(346.75) }
   end
