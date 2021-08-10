4d039ee02d2bab7bf077bc7abbb67d7568a1a101
David Schnee <david.schnee1@gmail.com>
2021-08-10 07:38:24 -0300

    feat/ create methods for authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+  before_action :authenticate_key
+
+  def authenticate_key
+    if current_api_key.nil?
+      render(json: { 'error' => 'Chave de API inválida' }, status: :unauthorized) && return
+    end
+  end
+
+  private
+
+  def sent_api_key
+    request.headers['X-Api-Key']
+  end
+
+  def current_api_key
+    ApiKey.find_by(key: sent_api_key)
+  end
 end
