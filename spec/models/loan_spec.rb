1319626934615139e9ccecbd8699ad2ae432ea51
pauloalchemist <paulolinsdev@gmail.com>
2021-08-22 18:42:17 -0300

    do not save if value for financed amount is equal to zero

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -6,5 +6,10 @@ RSpec.describe Loan, type: :model do
       loan = Loan.new(financed_amount: 1000, rate: 2, months: 12)
       expect(loan.save).to be(true)
     end
+
+    it 'do not save if value for financed amount is equal to zero' do
+      loan = Loan.new(financed_amount: 0, rate: 2, months: 12)
+      expect(loan.save).to be(false)
+    end
   end
 end
