f389203353fc1027cae5f24c99280e3a748545eb
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-08-22 22:15:34 -0300

    build: implemented migrate for loans

---
+++ db/migrate/20210821091757_create_loans.rb
@@ -0,0 +1,10 @@
+class CreateLoans < ActiveRecord::Migration[6.1]
+  def change
+    create_table :loans do |t|
+      t.decimal :value, null: false
+      t.float :rate, null: false
+      t.integer :installments, null: false, default: 12
+      t.timestamps
+    end
+  end
+end
