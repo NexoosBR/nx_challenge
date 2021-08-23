e9ecf2ecf14ebed9a2835d1a3cb7cc8e78bc0a54
LucianoPaulista <lucianodiaspaulista@gmail.com>
2021-08-22 22:11:40 -0300

    feat: changed loans controller

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,10 +1,40 @@
 class LoansController < ApplicationController
+  before_action :set_loan, only: %i[ show ]
+
   def create
-    render json: { loan: { id: 1 } }
+    loan = Loan.create(value: loan_params[:value], rate: loan_params[:taxa])
+    if loan.save
+      render json: { loan: { id: loan.id } }
+    else
+      render json: { message: { errors: "Unable to create the loan"} }
+    end
   end
 
   def show
-    pmt =  3_700 / 12
-    render json: { loan: { id: 1, pmt: pmt } }
+    pmt = @loan.calculate_pmt
+    render json: { loan: { id: @loan.id, pmt: pmt } }
   end
+
+  private
+  def loan_params
+    params.permit(:value, :taxa)
+  end
+
+  def set_loan
+    begin
+     @loan = Loan.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      render json: { message: { errors: "Loan not found" } }
+    rescue ActiveRecord::ActiveRecordError
+      # handle other ActiveRecord errors
+    rescue # StandardError
+      # handle most other errors
+    rescue Exception
+      # handle everything else
+      raise
+    end
+
+  end
+
+
 end
