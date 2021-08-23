f4e641b2188155356490eec117842a81895d4af6
pauloalchemist <paulolinsdev@gmail.com>
2021-08-22 23:20:04 -0300

    added test for def show

--- spec/controllers/loans_controllers_spec.rb
+++ spec/controllers/loans_controllers_spec.rb
@@ -1,19 +1,11 @@
-require "rails_helper" 
+require 'rails_helper'
 
-RSpec.describe LoansController do
-  describe "GET show" do
-    it do
-      get :show, params: { id: 1 }
-      param = JSON.parse(response.body).with_indifferent_access
-      expect(param[:loan][:id]).to(eq(1))
-    end
-  end
-
-  describe "POST show" do
-    it do
-      post :create
-      param = JSON.parse(response.body).with_indifferent_access
-      expect(param[:loan][:id]).to(eq(2))
+RSpec.describe LoansController, type: :request do
+  context 'requests' do
+    it 'Show loan by id' do
+      get '/loans/45'
+      expect(response).to have_http_status(:success)
+      expect(JSON.parse(response.body.size)).to eq(2)
     end
   end
 end
