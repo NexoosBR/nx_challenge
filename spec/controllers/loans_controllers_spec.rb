9f99dce1e06f3ea40753ad49c618423023aa9b82
vapordev <wagner.dev1337@gmail.com>
2021-08-27 22:24:52 -0300

    rubocop offenses

--- spec/controllers/loans_controllers_spec.rb
+++ spec/controllers/loans_controllers_spec.rb
@@ -3,26 +3,26 @@
 require "rails_helper"
 
 RSpec.describe Api::V1::LoansController, type: :request do
-  context 'payments' do
-    it 'should return status 200' do
+  context "payments" do
+    it "should return status 200" do
       get api_v1_url(:payments)
       expect(response).to have_http_status(200)
     end
 
-    it 'should have the same id' do
+    it "should have the same id" do
       get api_v1_url(:payments), params: {id: 1}
       param = JSON.parse(response.body).with_indifferent_access
       expect(param[:loan][:id]).to(eq(1))
     end
   end
 
-  context 'make_loan' do
-    it 'should return status 200' do
+  context "make_loan" do
+    it "should return status 200" do
       post api_v1_loans_url(:make_loan), params: {present_value: 100, tax: 0.2}
       expect(response).to have_http_status(200)
     end
 
-    it 'should have the same id' do
+    it "should have the same id" do
       post api_v1_loans_url(:make_loan), params: {present_value: 100, tax: 0.2}
       param = JSON.parse(response.body).with_indifferent_access
       expect(param[:loan][:id]).to(eq(2))
