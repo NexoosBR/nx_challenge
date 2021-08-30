e2a5c9e9a7516661a0a594d55616e49622df8b47
andre-dan <andrerndc@gmail.com>
2021-08-30 11:24:11 -0300

    Adicionando Controller Loan

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,10 +1,30 @@
+# frozen_string_literal: true
+
 class LoansController < ApplicationController
   def create
-    render json: { loan: { id: 1 } }
+    loan = Loan.create(amount: loan_params[:value], rate: loan_params[:taxa])
+   
+    if loan.valid?
+      render json: { loan: { id: loan.id } }
+    else
+      render json: { message: { errors: loan.errors.full_messages } }
+    end
+
   end
 
   def show
-    pmt =  3_700 / 12
-    render json: { loan: { id: 1, pmt: pmt } }
+    loan = Loan.where(id: params[:id]).first
+    if loan.present?
+      pmt = loan.calc_pmt
+      render json: { loan: { id: params[:id], pmt: pmt } }
+    else
+      render json: { message: { errors: 'Loan not found' } }
+    end
+  end
+
+  private
+
+  def loan_params
+    params.permit(:value, :taxa)
   end
 end
