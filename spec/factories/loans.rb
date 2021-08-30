c2f4ac6fd2d7dbb1e6c0b28e1150b64eb0b1c183
andre-dan <andrerndc@gmail.com>
2021-08-30 11:23:52 -0300

    Configurando os testes com Rspec

---
+++ spec/factories/loans.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+    factory :loan do
+        amount {3500} 
+        rate {2.0}
+        installments {3}
+    end
+  end 
---
+++ spec/models/loan_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Loan, type: :model do
+  let(:loan) { build :loan }
+
+  it 'calculate parcel PMT' do
+    loan.save
+
+    expect(loan.pmt.to_f).to eq(1213.64)
+  end
+end
