4342a1063bc834ff43152b6b014f4a035beb9775
Fabricio Bonjorno <fabricio.bonjorno@gmail.com>
2021-08-31 23:32:53 -0400

    Adjusted controller and validations

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,10 +1,26 @@
 class LoansController < ApplicationController
+  before_action :load_loan, only: %i[show]
+
   def create
-    render json: { loan: { id: 1 } }
+    @loan = Loan.new(loan_params)
+    if @loan.save
+      render json: { message: 'Successfully generated loan' }, status: :created # 201
+    else
+      render json: { errors: @loan.errors.full_messages }, status: :bad_request # 400
+    end
   end
 
   def show
-    pmt =  3_700 / 12
-    render json: { loan: { id: 1, pmt: pmt } }
+    render json: @loan.to_json(except: [:created_at, :updated_at])
+  end
+
+  private
+
+  def loan_params
+    params.require(:loan).permit(:present_value, :rate, :period, :amount)
+  end
+
+  def load_loan
+    @loan = Loan.find(params[:id])
   end
 end
