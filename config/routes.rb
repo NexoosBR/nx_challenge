e6b697d1b692f6429763bc3227ecf7b15019257a
Victor Hugo Albuquerque <vh12albuuerque@gmail.com>
2021-09-08 10:54:22 -0300

    alter routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  resources :loans
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :loans, only: [:show, :create] do
-  end
+  resources :loans, only: [:show, :create]
 end
