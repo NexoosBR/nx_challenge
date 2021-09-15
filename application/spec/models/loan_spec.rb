d5add66bb505014a836ee33f7eb955cae2bf1e6c
Jhonatta Santos <jhonattasantoss@hotmail.com>
2021-09-15 00:12:05 -0300

    feat: add unit test

--- application/spec/models/loan_spec.rb
+++ application/spec/models/loan_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Loan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'pmt' do
+    loan = build(:loan)
+    expect(loan.pmt).to eq(2334.7641675073237)
+  end
+
+  context 'when is invalid' do
+    it 'rate is zero' do
+      loan = build(:loan)
+      loan.rate = 0
+      expect(loan.valid?).to eq(false)
+    end
+
+    it 'installments is zero' do
+      loan = build(:loan)
+      loan.installments = 0
+      expect(loan.valid?).to eq(false)
+    end
+  end
+
 end
