6ead9d4119077b9caa701292be31a7c112320c53
Juliano Augusto <julianoaugusto22@gmail.com>
2021-09-14 23:20:54 -0300

    definira as rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :loans, only: [:show, :create] do
+  namespace 'v1' do
+    resources :loans, only: [:show, :create]
   end
 end
