c23000b8be57a8a8f46d411bb85e61dedb67bc4d
[Gustavo Gama] <[gutomgama@gmail.com]>
2021-09-23 18:17:39 -0300

    loan controller

--- app/controllers/loans_controller.rb
+++ app/controllers/loans_controller.rb
@@ -1,18 +1,26 @@
 class LoansController < ApplicationController
+  before_action :set_loan_id, only: %i[ show ]
   def create
     @loan = Loan.new(loan_params)
-    return render json: @loan.errors, status: :unprocessable_entity unless @loan.save
-
-    render json: { loan: { id: @loan.id } }, status: :created
+    if @loan.save
+      render json: { loan: { id: @loan.id } }, status: :created
+    else
+      return render json: @loan.errors, status: :unprocessable_entity
+    end    
   end
 
   def show
-    @loan = Loan.find_by(id: params[:id])
-    return render json: { message: ('not found') }, status: :not_found if @loan.nil?
-    render json: { loan: { id: @loan.id, pmt: @loan.pmt } }
+    if @loan.nil? == false
+      render json: { loan: { id: @loan.id, pmt: @loan.pmt } }
+    else
+      return render json: { message: ('not found') }, status: :not_found
+    end    
   end
 
   private
+  def set_loan_id
+    @loan = Loan.find_by(id: params[:id])
+  end
 
   def loan_params
     params.require(:loan).permit(:value, :taxa, :pmt)
