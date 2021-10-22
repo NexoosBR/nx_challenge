8a47efeb3b30e64017abab225069879ad4352349
Danilo Castro <danilogcastro@hotmail.com>
2021-10-21 22:20:58 -0300

    adicionado callback no modelo que calcula o PMT antes de salvar cada objeto

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,2 +1,12 @@
 class Loan < ApplicationRecord
+  before_save :set_pmt
+
+  private
+
+  def set_pmt
+    value = self.value
+    fee = self.fee
+    months = self.months
+    self.pmt = Pmt::Calculator.call(value, fee, months).round(2)
+  end
 end
