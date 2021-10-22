79f701bff1513e4cc3ec8e48343368e0558258a9
tarique vieira ramos <tariquevieira@gmail.com>
2021-10-22 07:03:47 -0300

    refactor(rwag): remove gem rswag

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -1,4 +1,4 @@
 class Loan < ApplicationRecord
-  validates :value, presence: true
-  validates :taxa, presence: true
+  validates :value, presence: true,numericality: true
+  validates :taxa, presence: true,numericality: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  mount Rswag::Ui::Engine => '/api-docs'
-  mount Rswag::Api::Engine => '/api-docs'
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
