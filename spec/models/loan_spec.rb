6528e0614566d24644452b9f2ea842522ba54a79
Danilo Castro <danilogcastro@hotmail.com>
2021-10-21 22:45:09 -0300

    Teste para checar se pmt está sendo calculada corretamente

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Loan, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "deve calcular a PMT corretamente" do
+    loan = Loan.new(value: 5000, months: 24, fee: 11)
+    loan.save
+    expect(loan.pmt.to_f).to eq(598.94)
+  end
 end
